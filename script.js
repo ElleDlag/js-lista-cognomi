78ad72670a14f51712f62213a3ad1109a424099f
ElleDlag <38381770+ElleDlag@users.noreply.github.com>
2019-10-23 17:12:53 +0200

    semicolon correction

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ console.log(surname);
 var mySurnamePos = surname.indexOf(mySurname);
 console.log(mySurnamePos);
 
-var output = document.querySelector('.list-name')
+var output = document.querySelector('.list-name');
 
 for (var i = 0; i<surname.length; i++ ){
     output.textContent += surname[i] + ", ";
-}
+};
 
-document.querySelector('.position-name').textContent = mySurnamePos
+document.querySelector('.position-name').textContent = mySurnamePos;
 
 
 
