261c90e57056a07bb19179410e8d39be974649ec
ElleDlag <38381770+ElleDlag@users.noreply.github.com>
2019-10-23 17:05:59 +0200

    init and finish ex base

---
+++ script.js
@@ -0,0 +1,22 @@
+var surname = ["sacchi", "speranza","neri", "bianchi","miele","ferrari",  "conte", "rossi","ferro", "sasso", ];
+console.log(surname);
+var mySurname = prompt("Your surname");
+console.log(mySurname);
+surname.push(mySurname);
+console.log(surname);
+surname.sort();
+console.log(surname);
+var mySurnamePos = surname.indexOf(mySurname);
+console.log(mySurnamePos);
+
+var output = document.querySelector('.list-name')
+
+for (var i = 0; i<surname.length; i++ ){
+    output.textContent += surname[i] + ", ";
+}
+
+document.querySelector('.position-name').textContent = mySurnamePos
+
+
+
+
